d85b8a347a371cac6cf3b2134c7162dcb268ae25
Jerry Eduardo <jerry.edu@gmail.com>
2024-04-24 21:28:21 -0300

    Atualização

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
 $('.nav-link').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+$('.nav-link').on('click', function(event){
+    var target = $(this.getAttribute('href'));
+    if(target.length){
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - $('.navbar').height()
+        }, 1000);
+    }
+});
